894311b975b3869657f6131084cacc0e53354385
JasonSchramm <jasonschramm@ufl.edu>
2021-01-19 21:21:48 -0500

    write to google database (poorly)

--- app/src/main/java/com/example/uf_schedule_app/MainActivity.java
+++ app/src/main/java/com/example/uf_schedule_app/MainActivity.java
@@ -21,19 +21,23 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.DiskBasedCache;
 import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
-import java.nio.Buffer;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
@@ -47,28 +51,44 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
         // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
         String url ="https://one.ufl.edu/apix/soc/schedule/?category=RES&term=2211";
+        RequestQueue queue = Volley.newRequestQueue(this);
 
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
+        // prepare the Request
+        JsonArrayRequest getRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>()
+                {
                     @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        //textView.setText("Response is: "+ response.substring(0,500));
-                        System.out.println("Response is: "+ response.toString());
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                //textView.setText("That didn't work!");
-                System.out.println("That didn't work!");
-            }
-        });
+                    public void onResponse(JSONArray response) {
+                        // display response
+                        System.out.println("Response JSON" + response.toString());
 
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+                        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+                        try {
+                            ArrayList<String> listdata = new ArrayList<String>();
+                            if (response != null) {
+                                for (int i=0;i<response.length();i++){
+                                    listdata.add(response.getString(i));
+                                }
+                            }
+                            System.out.println("ListData: " + listdata.toString());
+                            mDatabase.setValue(listdata);
+                        } catch (Exception e) {
+                            System.out.println("Error Caught: " + e);
+                        }
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println("Error.Response" + error.toString());
+                    }
+                }
+        );
+
+        // Access the RequestQueue through your singleton class.
+        queue.add(getRequest);
 
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
