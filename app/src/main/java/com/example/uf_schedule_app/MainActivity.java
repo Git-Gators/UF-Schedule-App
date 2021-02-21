ee779034265985cc6744d46269c8ff1dbe1ce47e
Michael Lee <michael.lee@ufl.edu>
2021-02-20 21:13:23 -0500

    Testing co-author commit\n\nCo-authored-by: Matthew_Flippen mflippen@ufl.edu \n Co-authored-by: Trevor_Vente tvente@ufl.edu

--- app/src/main/java/com/example/uf_schedule_app/MainActivity.java
+++ app/src/main/java/com/example/uf_schedule_app/MainActivity.java
@@ -140,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
         loginPopup_SignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Add sign-in functionality here.
+                /**Add sign-in functionality here*/
                 dialog.dismiss();
             }
         });
