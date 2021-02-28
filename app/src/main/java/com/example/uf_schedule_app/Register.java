18aeec39d7a93334c152a4fcdfa2305b40d33511
Matthew <flpn4life@gmail.com>
2021-02-28 07:27:26 -0500

    Changes to Login and Gradle
    User Registration now adds user object with login id to database.  Currently contains user's name and email address; this can be changed in the future.
    
    Also made changes to Gradle to get it to compile; needed to add mulidex to it so that we didn't exceed the number of libraries we're allowed to call.

--- app/src/main/java/com/example/uf_schedule_app/Register.java
+++ app/src/main/java/com/example/uf_schedule_app/Register.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,9 +15,18 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
     FirebaseAuth fAuth;
+    FirebaseFirestore userdb = FirebaseFirestore.getInstance();
+    String userId;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     EditText registerFullName, registerEmail, registerPassword, registerConfPassword;
     Button registerBtn;
@@ -44,6 +54,8 @@ public class Register extends AppCompatActivity {
                 String password = registerPassword.getText().toString();
                 String confPassword = registerConfPassword.getText().toString();
 
+
+
                 if(fullName.isEmpty()){
                     registerFullName.setError("Full Name is Required");
                     return;
@@ -68,6 +80,20 @@ public class Register extends AppCompatActivity {
                     @Override
                     public void onSuccess(AuthResult authResult) {
                         //send user to home page on success
+                        userId = fAuth.getCurrentUser().getUid();
+                        DocumentReference documentReference = userdb.collection("users").document(userId);
+
+                        Map<String, Object> user = new HashMap<>();
+                        user.put("Full Name", fullName);
+                        user.put("Email", email);
+
+                        documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "onSucess: user profile is created for " + userId);
+                            }
+                        });
+
                         startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         finish();
 
@@ -78,6 +104,10 @@ public class Register extends AppCompatActivity {
                         Toast.makeText(Register.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
+
+                Map<String, Object> user = new HashMap<>();
+                user.put("fullname", fullName);
+                user.put("email", email);
             }
         });
     }
