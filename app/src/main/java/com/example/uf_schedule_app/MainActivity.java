53f3b4eb661b53659aa3ec88f0094517d9cfd1a5
cncornejo <ccornejo@ufl.edu>
2021-03-19 13:07:17 -0400

    Added email validation, password change, and forgot password reset link

--- app/src/main/java/com/example/uf_schedule_app/MainActivity.java
+++ app/src/main/java/com/example/uf_schedule_app/MainActivity.java
@@ -6,10 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.InputType;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -52,9 +57,11 @@ public class MainActivity extends AppCompatActivity {
 
     private AlertDialog.Builder dialogBuilder;
     private AlertDialog dialog;
-    private TextView loginPopup_title;
-    private Button loginPopup_SignIn, loginPopup_create_account, loginBtnHomePage, logoutBtnHomePage;
+    private TextView loginPopup_title, verifyMsg;
+    private Button loginPopup_SignIn, loginPopup_create_account, loginBtnHomePage, logoutBtnHomePage, verifyEmailBtn, loginPopupForgotPasswordBtn;
     private EditText loginPopup_email, loginPopup_password;
+    AlertDialog.Builder reset_alert;
+    LayoutInflater loutInflater;
 
 
 
@@ -80,30 +87,30 @@ public class MainActivity extends AppCompatActivity {
 
         //If we're coming from the filter, we grab the info
         Bundle b = getIntent().getExtras();
-        if(b != null){
-            if(b.getStringArrayList("coursesPicked") != null){
+        if (b != null) {
+            if (b.getStringArrayList("coursesPicked") != null) {
                 coursesPicked = b.getStringArrayList("coursesPicked");
                 ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, coursesPicked);
                 chosenCourses.setAdapter(arrayAdapter);
             }
-            if(b.getStringArrayList("courses") != null){
+            if (b.getStringArrayList("courses") != null) {
                 courses.remove("");
                 courses = b.getStringArrayList("courses");
                 ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, courses);
                 courseList.setAdapter(arrayAdapter);
             }
-            if(b.getString("course") != null){
-                if(!b.getString("course").equals("")) {
+            if (b.getString("course") != null) {
+                if (!b.getString("course").equals("")) {
                     courses.add(b.getString("course"));
                     ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, courses);
                     courseList.setAdapter(arrayAdapter);
                 }
             }
-            if(b.getString("semester") != null){
+            if (b.getString("semester") != null) {
                 //
             }
-            if(b.getString("department") != null){
-               department = b.getString("department");
+            if (b.getString("department") != null) {
+                department = b.getString("department");
             }
         }
 
@@ -119,13 +126,43 @@ public class MainActivity extends AppCompatActivity {
         courseList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if(coursesPicked.size() < 4){
+                if (coursesPicked.size() < 4) {
                     coursesPicked.add(courses.get(position));
                     ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, coursesPicked);
                     chosenCourses.setAdapter(arrayAdapter);
                 }
             }
         });
+
+        verifyMsg = findViewById(R.id.verifyEmailMsg);
+        verifyEmailBtn = findViewById(R.id.verifyEmailBtn);
+        fAuth = FirebaseAuth.getInstance();
+        reset_alert = new AlertDialog.Builder(this);
+        loutInflater = this.getLayoutInflater();
+
+        if(FirebaseAuth.getInstance().getCurrentUser() != null) {
+
+            if (!fAuth.getCurrentUser().isEmailVerified()) {
+                verifyMsg.setVisibility(View.VISIBLE);
+                verifyEmailBtn.setVisibility(View.VISIBLE);
+
+            }
+
+            verifyEmailBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //send verification email
+                    fAuth.getCurrentUser().sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(MainActivity.this, "Verification Email Sent", Toast.LENGTH_SHORT).show();
+                            verifyMsg.setVisibility(View.GONE);
+                            verifyEmailBtn.setVisibility(View.GONE);
+                        }
+                    });
+                }
+            });
+        }
     }
 
 
@@ -151,6 +188,8 @@ public class MainActivity extends AppCompatActivity {
             loginPopup_password = (EditText) LoginPopupView.findViewById(R.id.input_password);
             loginPopup_SignIn = (Button) LoginPopupView.findViewById(R.id.sign_in_button);
             loginPopup_create_account = (Button) LoginPopupView.findViewById(R.id.create_account_button);
+            loginPopupForgotPasswordBtn = (Button) LoginPopupView.findViewById(R.id.forgotPasswordBtn);
+
 
             dialogBuilder.setView(LoginPopupView);
             dialog = dialogBuilder.create();
@@ -195,6 +234,43 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(new Intent(getApplicationContext(), Register.class));
                 }
             });
+
+            loginPopupForgotPasswordBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //start alert dialog
+                    View v = loutInflater.inflate(R.layout.forgot_password_email_form_popup, null);
+                    reset_alert.setTitle("Forgot Password")
+                            .setMessage("Enter Email for Password Reset Link")
+                            .setPositiveButton("Reset", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    //validate email address
+                                    EditText email = v.findViewById(R.id.forgot_password_email_popup);
+                                    if(email.getText().toString().isEmpty()){
+                                        email.setError("Required");
+                                        return;
+                                    }
+                                    //send reset link
+                                    fAuth.sendPasswordResetEmail(email.getText().toString()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Toast.makeText(MainActivity.this, "Reset Email Sent", Toast.LENGTH_SHORT).show();
+
+                                        }
+                                    }).addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                                        }
+                                    });
+                                }
+                            }).setNegativeButton("Cancel", null)
+                            .setView(v)
+                            .create().show();
+                }
+            });
         }
 
         private BottomNavigationView.OnNavigationItemSelectedListener navListener =
@@ -236,6 +312,7 @@ public class MainActivity extends AppCompatActivity {
             logoutBtnHomePage.setVisibility(View.VISIBLE);
 
 
+
         } else {
             loginBtnHomePage.setVisibility(View.VISIBLE);
             logoutBtnHomePage.setVisibility(View.GONE);
@@ -250,5 +327,24 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.settings_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.resetPassword){
+            if(FirebaseAuth.getInstance().getCurrentUser() != null){
+                startActivity(new Intent(getApplicationContext(), ResetPassword.class));
+            }
+            else {
+                Toast.makeText(MainActivity.this, "Please Login First", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
